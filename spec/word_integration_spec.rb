aa1b223e7f679f4b1ad62c182e377a8f2b250848
Grady L Shelton <gradyish@gmail.com>
2017-04-28 14:14:22 -0700

    pass integration spec showing photo from url input. set max width of photos to 600 px

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -7,7 +7,7 @@ describe("the word creation path", {:type => :feature}) do
   it("processes the user input and adds word to words array") do
     visit("/word/new")
     fill_in("spelling", :with => "yolo")
-    fill_in("picture", :with => "dogpic")
+    fill_in("picture", :with => "https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg")
     click_button("Add Word")
     expect(page).to have_content("Stupendously Awesome")
   end
@@ -18,6 +18,13 @@ describe("the word view path", {:type => :feature}) do
     visit("/1")
     expect(page).to have_content("yolo")
   end
+
+describe("the photo view path", {:type => :feature}) do
+  it("allows user to view definition page of a word") do
+    visit("/1")
+    expect(page).to have_content("Here's a picture of your word")
+  end
+end
 
   it("processes the user input and adds definitions to a word") do
     visit("/1/definition/new")
