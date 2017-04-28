03db067cf777eeab0082c8c1269ddc23c27961cd
Grady L Shelton <gradyish@gmail.com>
2017-04-28 08:50:08 -0700

    pass .clear and .all tests to show all items in the definition array and to clear them out properly

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,6 +2,9 @@ require('rspec')
 require('definition')
 
 describe(Definition) do
+  before() do
+    Definition.clear()
+  end
 
   describe("#meaning") do
     it("will initialize a definition") do
@@ -11,10 +14,24 @@ describe(Definition) do
   end
 
   describe("#save") do
-  it("will add a new definition to definitions array") do
-   test_definition = Definition.new({:meaning => 'a state of mind where someone is disoriented and potentially hyper aware of their surroundings'})
-   test_definition.save()
-   expect((Definition.all)).to(eq([test_definition]))
+    it("will add a new definition to definitions array") do
+     test_definition = Definition.new({:meaning => 'a state of mind where someone is disoriented and potentially hyper aware of their surroundings'})
+     test_definition.save()
+     expect((Definition.all)).to(eq([test_definition]))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Definition.all()).to(eq([]))
+    end
+  end
+
+  describe(".clear") do
+  it("empties out all of the saved definitions") do
+    Definition.new({:meaning => 'a state of mind where someone is disoriented and potentially hyper aware of their surroundings'})
+    Definition.clear()
+    expect(Definition.all()).to(eq([]))
   end
 end
 end
