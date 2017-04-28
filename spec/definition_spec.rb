c5431fff0f1191b4ec41e0ed5323eaccf406afe6
Grady L Shelton <gradyish@gmail.com>
2017-04-28 08:46:28 -0700

    pass second unit test for definitions to save a new definition to the definition array

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -5,8 +5,16 @@ describe(Definition) do
 
   describe("#meaning") do
     it("will initialize a definition") do
-      test_contact = Definition.new({:meaning => 'a state of mind where someone is disoriented and potentially hyper aware of their surroundings'})
-      expect((test_contact.meaning)).to(eq("a state of mind where someone is disoriented and potentially hyper aware of their surroundings"))
+      test_definition = Definition.new({:meaning => 'a state of mind where someone is disoriented and potentially hyper aware of their surroundings'})
+      expect((test_definition.meaning)).to(eq("a state of mind where someone is disoriented and potentially hyper aware of their surroundings"))
     end
   end
+
+  describe("#save") do
+  it("will add a new definition to definitions array") do
+   test_definition = Definition.new({:meaning => 'a state of mind where someone is disoriented and potentially hyper aware of their surroundings'})
+   test_definition.save()
+   expect((Definition.all)).to(eq([test_definition]))
+  end
+end
 end
